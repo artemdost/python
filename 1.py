f4058ffaf3c58ba13737d168aa116feb7a977551
artemdost <dartem925@gmail.com>
2022-10-05 18:26:07 +0300

    Zadachi

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
-name = "Артём"
-fam = "Досталев"
-date = 2004
+name = input("Введите имя: ")
+fam = input("Введите фамилию: ")
+date = int(input("Введите год рождения: "))
 print(name,fam,date, sep='_')
 name, fam = fam, name
 date = 2004 + 60
