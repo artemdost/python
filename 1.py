04f6f48e7c9f054c04fc120c2e19f4556cfba5c8
artemdost <85932516+artemdost@users.noreply.github.com>
2022-10-13 12:55:54 +0300

    22БИ1_Досталев Артём_СР5

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ n = int(input("Укажите размерность массива: "))
 m = [0] * n
 print("Введите", n, "чисел / числа: ")
 index = 100000
-mmax = -100000000
+mmax = -1000000000
 for i in range(n):
     m[i] = float(input())
     if m[i] > mmax:
@@ -11,3 +11,4 @@ for i in range(n):
 for i in range(index+1, n):
     m[i] = float(0)
 print(*m)
+
