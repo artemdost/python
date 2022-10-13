83918327eb2f8227d3f0f82a004d7e715603feee
artemdost <85932516+artemdost@users.noreply.github.com>
2022-10-13 12:54:08 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,11 +1,13 @@
 n = int(input("Укажите размерность массива: "))
 m = [0] * n
 print("Введите", n, "чисел / числа: ")
+index = 100000
 mmax = -100000000
 for i in range(n):
     m[i] = float(input())
     if m[i] > mmax:
         mmax = m[i]
-    else:
-        m[i] = float(0)
+        index = i
+for i in range(index+1, n):
+    m[i] = float(0)
 print(*m)
