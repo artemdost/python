50913ac7249f52bc1c5aca3bd0a0a5ce08adbe22
artemdost <85932516+artemdost@users.noreply.github.com>
2022-10-19 16:37:25 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,14 +1,38 @@
-n = int(input("Укажите размерность массива: "))
-m = [0] * n
-print("Введите", n, "чисел / числа: ")
-index = 100000
-mmax = -1000000000
-for i in range(n):
-    m[i] = float(input())
-    if m[i] > mmax:
-        mmax = m[i]
-        index = i
-for i in range(index+1, n):
-    m[i] = float(0)
-print(*m)
-
+def translate(x, i):
+    x1 = int(x)
+    x2 = x % 1
+    s1 = ''
+    s2 = ''
+    while x1 > 0:
+        s1 = str(x1 % i) + s1
+        x1 = x1 // i
+    if i == 2:
+        for j in range(n):
+            x2 = x2 * 2
+            if x2 > 1:
+                s2 += '1'
+                x2 = x2 % 1
+            elif x2 == 1:
+                s2 += '1'
+                break
+            else:
+                s2 += '0'
+    if i == 8:
+        for j in range(n):
+            x2 = x2 * 8
+            if x2 > 1:
+                s2 += str(int(x2))
+                x2 = x2 % 1
+            elif x2 == int(x2):
+                s2 += str(int(x2))
+                break
+            else:
+                s2 += '0'
+    s = s1 +'.' + s2
+    print("Число после перевода:", float(s))
+        
+    
+x = float(input("Введите число: "))
+n = int(input("Введите кол-во знаков после запятой: "))
+i = int(input("Укажите систему счисления, в которую осуществить перевод (2 или 8): "))
+translate(x,i)
